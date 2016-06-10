a1c3b5abf4af039b3f1de0e2a9b791eae921cbbe
GunnerJnr <gunnerjnr@live.co.uk>
2016-06-10 21:11:48 +0100

    Added basic contact us page (html only)
    Added basic HTML design for contact us page, next will hopefully add
    some angular js functionality to store the data input

--- Stream 1/Final Project/Band-Project/js/app.js
+++ Stream 1/Final Project/Band-Project/js/app.js
@@ -1,20 +1,24 @@
 // define our application
 angular.module("bandApp", ['ngRoute', 'bandAppControllers', 'bandAppDirectives'])
-	.config(function ($routeProvider) {
-		$routeProvider
-			.when('/', {
-				templateUrl: "templates/home.html",
-				controller: "HomeController"
-			})
-			.when('/our-music', {
-				templateUrl: "templates/our-music.html",
-				controller: "AudioController"
-			})
-			.when('/searchiTunes', {
-				templateUrl: "templates/search-itunes.html",
-				controller: "itunesController"
-			})
-			.otherwise({
-				redirectTo: '/'
-			});
-	});
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: "templates/home.html",
+                controller: "HomeController"
+            })
+            .when('/our-music', {
+                templateUrl: "templates/our-music.html",
+                controller: "AudioController"
+            })
+            .when('/searchiTunes', {
+                templateUrl: "templates/search-itunes.html",
+                controller: "itunesController"
+            })
+            .when('/contact', {
+                templateUrl: "templates/contact.html",
+                controller: "ContactController"
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
