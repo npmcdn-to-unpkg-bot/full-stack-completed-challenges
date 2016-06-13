fd8f17ffa834111306a09cf781002933759d7876
GunnerJnr <gunnerjnr@live.co.uk>
2016-06-13 12:16:56 +0100

    Stream 1 Project Code Update
    Major Revision of the style.css file
    
    Commented all code clearly and tidily, removed code that didn't
    necessarily do anything or was not supposed to be there, project looking
    very smart at present, left to do is the modal for the booking page and
    images for the album art need renaming to friendlier names and possibly
    changing to something a bit more relevant perhaps the same can be said
    for the provided audio files

--- Stream 1/Final Project/Band-Project/js/app.js
+++ Stream 1/Final Project/Band-Project/js/app.js
@@ -1,28 +1,32 @@
 // define our application
 angular.module("bandApp", ['ngRoute', 'bandAppControllers', 'bandAppDirectives'])
-    .config(function ($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: "templates/home.html",
-                controller: "HomeController"
-            })
-            .when('/meet-the-band', {
-                templateUrl: "templates/meet-the-band.html",
-                controller: "AudioController"
-            })
-            .when('/tour-info', {
-                templateUrl: "templates/tour-info.html",
-                controller: "TourController"
-            })
-            .when('/services', {
-                templateUrl: "templates/services.html",
-                controller: "ContactController"
-            })
-            .when('/searchiTunes', {
-                templateUrl: "templates/search-itunes.html",
-                controller: "ItunesController"
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-    });
+	.config(function ($routeProvider, $locationProvider) {
+		// html5 Mode set to true enables friendly url's (currently only works in local server)
+		$locationProvider.html5Mode(true);
+
+		// route our application
+		$routeProvider
+			.when('/', {
+				templateUrl: "templates/home.html",
+				controller: "HomeController"
+			})
+			.when('/meet-the-band', {
+				templateUrl: "templates/meet-the-band.html",
+				controller: "AudioController"
+			})
+			.when('/tour-info', {
+				templateUrl: "templates/tour-info.html",
+				controller: "TourController"
+			})
+			.when('/services', {
+				templateUrl: "templates/services.html",
+				controller: "ContactController"
+			})
+			.when('/searchiTunes', {
+				templateUrl: "templates/search-itunes.html",
+				controller: "ItunesController"
+			})
+			.otherwise({
+				redirectTo: '/'
+			});
+	});
